aa2da896f89338cd6181e4e21ae3d9758c43d531
balbader <basilalbapro@gmail.com>
2023-05-01 15:52:36 +0200

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -14,12 +14,22 @@
 
 int	main(void)
 {
-	t_data	img;
 	void	*mlx;
+	void	*win;
 
 	mlx = mlx_init();
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-							&img.line_length, &img.endian);
+	if (!mlx)
+		return (MLX_ERROR);
+	win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "42 So_Long");
+	if (!win)
+	{
+		free(win);
+		return (MLX_ERROR);
+	}
+	while (1)
+		;
+	mlx_destroy_window(mlx, win);
+	mlx_destroy_display(mlx);
+	free(mlx);
 	return (0);
 }
