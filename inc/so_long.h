2a7daf2c0bb4b000cda3d4fd0cd4e5a01b902854
balbader <basilalbapro@gmail.com>
2023-05-01 15:04:22 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -18,4 +18,13 @@
 #include <X11/keysym.h>
 #include <math.h>
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
 #endif
