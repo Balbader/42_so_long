b108734b1f3c8577311a765799d171334b0d056b
Basil ALBADER <baalbade@student.42.fr>
2023-06-01 16:42:32 +0200

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -17,7 +17,7 @@ void	ft_mlx_put_pixel(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 int	ft_handle_keypress(int keysym, t_data *data)
@@ -30,11 +30,31 @@ int	ft_handle_keypress(int keysym, t_data *data)
 	return (0);
 }
 
+int	ft_render_rect(t_data *data, t_rect rect)
+{
+	int	i;
+	int	j;
+
+	if (data->win == NULL)
+		return (1);
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			mlx_pixel_put(data->mlx, data->win, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
 int	ft_render(t_data *data)
 {
-	if (data->win != NULL)
-		mlx_pixel_put(data->mlx, data->win, WINDOW_HEIGHT / 2,
-			WINDOW_HEIGHT / 2, RED_PIXEL);
+	// if (data->win != NULL)
+	// 	mlx_pixel_put(data->mlx, data->win, WINDOW_WIDTH / 2,
+	// 		WINDOW_HEIGHT / 2, RED_PIXEL);
+	ft_render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
+		100, 100, GREEN_PIXEL});
 	return (0);
 }
 
@@ -45,7 +65,8 @@ int	main(void)
 	data.mlx = mlx_init();
 	if (!data.mlx)
 		return (MLX_ERROR);
-	data.win = mlx_new_window(data.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "42 So_Long");
+	data.win = mlx_new_window(data.mlx, WINDOW_WIDTH,
+			WINDOW_HEIGHT, "42 So_Long");
 	if (!data.win)
 	{
 		free(data.win);
@@ -68,14 +89,16 @@ int	main(void)
 // 	mlx.mlx = mlx_init();
 // 	if (!mlx.mlx)
 // 		return (MLX_ERROR);
-// 	win.win = mlx_new_window(mlx.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "42 So_Long");
+// 	win.win = mlx_new_window(mlx.mlx, WINDOW_WIDTH,
+// 	WINDOW_HEIGHT, "42 So_Long");
 // 	if (!win.win)
 // 	{
 // 		free(win.win);
 // 		return (MLX_ERROR);
 // 	}
 // 	img_1.img = mlx_new_image(mlx.mlx, IMG_WIDTH, IMG_HEIGHT);
-// 	img_1.addr = mlx_get_data_addr(img_1.img, &img_1.bits_per_pixel, &img_1.line_length,
+// 	img_1.addr = mlx_get_data_addr(img_1.img,
+// 	&img_1.bits_per_pixel, &img_1.line_length,
 // 			&img_1.endian);
 // 	ft_mlx_put_pixel(&img_1, 5, 5, 0x00FF0000);
 // 	mlx_put_image_to_window(mlx.mlx, win.win, img_1.img, 0, 0);
