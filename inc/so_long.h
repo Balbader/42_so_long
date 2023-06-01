91ddf47bbdd76ec06843dd58f05f2ccb98ba361c
Basil ALBADER <baalbade@student.42.fr>
2023-06-01 16:42:19 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -20,6 +20,7 @@
 # define MLX_ERROR 1
 
 # define RED_PIXEL 0xFF0000
+# define GREEN_PIXEL 0xFF00
 
 #include "../libft/includes/libft.h"
 #include "../mlx_linux/mlx.h"
@@ -34,9 +35,23 @@ typedef struct s_data
 	void	*win;
 	void	*img;
 	char	*addr;
+	int		x;
+	int		y;
+	int		width;
+	int		height;
+	int		color;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
 }				t_data;
+
+typedef struct s_rect
+{
+	int		x;
+	int		y;
+	int		width;
+	int		height;
+	int		color;
+}				t_rect;
 
 #endif
