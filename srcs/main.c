aff28b8f4c7cae6b4b23081d7aadacfa8d7d693f
Basil ALBADER <baalbade@student.42.fr>
2023-06-15 17:29:02 +0200

    main.c update

--- srcs/main.c
+++ srcs/main.c
@@ -12,94 +12,7 @@
 
 #include "so_long.h"
 
-void	ft_mlx_put_pixel(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
-}
-
-int	ft_handle_keypress(int keysym, t_data *data)
-{
-	if (keysym == XK_Escape)
-	{
-		mlx_destroy_window(data->mlx, data->win);
-		data->win = NULL;
-	}
-	return (0);
-}
-
-void	ft_render_background(t_data *data, int color)
-{
-	int	i;
-	int	j;
-
-	if (data->win == NULL)
-		return ;
-	i = 0;
-	while (i < WINDOW_HEIGHT)
-	{
-		j = 0;
-		while (j < WINDOW_WIDTH)
-			mlx_pixel_put(data->mlx, data->win, j++, i, color);
-		++i;
-	}
-}
-
-int	ft_render_rect(t_data *data, t_rect rect)
-{
-	int	i;
-	int	j;
-
-	if (data->win == NULL)
-		return (1);
-	i = rect.y;
-	while (i < rect.y + rect.height)
-	{
-		j = rect.x;
-		while (j < rect.x + rect.width)
-			mlx_pixel_put(data->mlx, data->win, j++, i, rect.color);
-		++i;
-	}
-	return (0);
-}
-
-int	ft_render(t_data *data)
-{
-	// ft_render_background(data, WHITE_PIXEL);
-	ft_render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
-	ft_render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
-		100, 100, GREEN_PIXEL});
-	return (0);
-}
-
-int	ft_draw_line(t_data *data, int start_x, int start_y, int end_x, int end_y, int color)
-{
-	double	delta_x;
-	double	delta_y;
-	double	pixel_x;
-	double	pixel_y;
-	int		pixels;
-
-	delta_x = end_x - start_x;
-	delta_y = end_y - start_y;
-	pixels = sqrt((delta_x * delta_x) + (delta_y * delta_y));
-	delta_x /= pixels;
-	delta_y /= pixels;
-	pixel_x = start_x;
-	pixel_y = start_y;
-	while (pixels)
-	{
-		mlx_pixel_put(data->mlx, data->win, pixel_x, pixel_y, color);
-		pixel_x += delta_x;
-		pixel_y += delta_y;
-		--pixels;
-	}
-	return (0);
-}
-
-int	main(void)
+int	main(int ac, char **av)
 {
 	t_data	data;
 
@@ -113,10 +26,6 @@ int	main(void)
 		free(data.win);
 		return (MLX_ERROR);
 	}
-	ft_draw_line(&data, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0, 0xFFFFFF);
-	ft_draw_line(&data, WINDOW_WIDTH, 0, 0, WINDOW_HEIGHT, 0xFFFFFF);
-	ft_draw_line(&data, WINDOW_WIDTH, WINDOW_HEIGHT / 2, 0, WINDOW_HEIGHT / 2, 0xFFFFFF);
-	ft_render(&data);
 	mlx_loop_hook(data.mlx, &ft_render, &data);
 	mlx_hook(data.win, KeyPress, KeyPressMask, &ft_handle_keypress, &data);
 	mlx_loop(data.mlx);
@@ -124,33 +33,3 @@ int	main(void)
 	free(data.mlx);
 	return (0);
 }
-
-// int	main(void)
-// {
-	// t_data	img_1;
-	// t_data	mlx;
-	// t_data	win;
-
-	// mlx.mlx = mlx_init();
-	// if (!mlx.mlx)
-	// 	return (MLX_ERROR);
-	// win.win = mlx_new_window(mlx.mlx, WINDOW_WIDTH,
-	// WINDOW_HEIGHT, "42 So_Long");
-	// if (!win.win)
-	// {
-	// 	free(win.win);
-	// 	return (MLX_ERROR);
-	// }
-	// img_1.img = mlx_new_image(mlx.mlx, IMG_WIDTH, IMG_HEIGHT);
-	// img_1.addr = mlx_get_data_addr(img_1.img,
-	// &img_1.bits_per_pixel, &img_1.line_length,
-	// 		&img_1.endian);
-	// ft_mlx_put_pixel(&img_1, 5, 5, 0x00FF0000);
-	// mlx_put_image_to_window(mlx.mlx, win.win, img_1.img, 0, 0);
-	// mlx_loop(mlx.mlx);
-	// mlx_destroy_image(mlx.mlx, img_1.img);
-	// mlx_destroy_window(mlx.mlx, win.win);
-	// mlx_destroy_display(mlx.mlx);
-	// free(mlx.mlx);
-	// return (0);
-// }
