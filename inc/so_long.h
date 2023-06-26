2f6f7446d2fa48c946e63481e708fd9529f88293
balbader <basilalbapro@gmail.com>
2023-06-26 16:50:21 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -13,38 +13,57 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include "../libft/includes/libft.h"
 #include "../mlx_linux/mlx.h"
 #include <X11/keysym.h>
 #include <X11/X.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
+# include <stddef.h>
+# include <limits.h>
+# include <stdarg.h>
 
-typedef struct s_data
+typedef struct t_start
 {
-	void	*mlx;
-	void	*win;
-	void	*floor;
-	void	*block;
-	void	*player;
-	void	*collectible;
+	int		fd;
+	int		heightmap;
+	int		widthmap;
+	int		playercount;
+	int		columncount;
+	int		exitcount;
+	int		x_axis;
+	int		y_axis;
+	int		counter;
+	int		collectables;
 
 	char	**map;
 
-	int		x;
-	int		y;
-	int		width;
-	int		height;
-	int		fd;
-	int		player;
-	int		column;
-	int		exit;
-	int		counter;
-	int		collectibles;
-}				t_data;
+	void	*floor;
+	void	*barrier;
+	void	*player;
+	void	*exit;
+	void	*collectable;
+	void	*mlxpointer;
+	void	*winpointer;
 
+}	t_complete;
+
+int		exit_point(t_complete *game);
+int		map_reading(t_complete *game, char **argv);
+int		controls_working(int command, t_complete *game);
+void	adding_in_graphics(t_complete *game);
+void	place_images_in_game(t_complete *game);
+void	check_errors(t_complete *game);
+
+int		main(void);
+int		ft_printf(const char *str, ...);
+void	ft_print_nb(int nb, int *tot_len);
+void	ft_print_hexa(unsigned int nb, char c, int *tot_len);
+void	ft_print_unsigned(unsigned int u, int *tot_len);
+void	ft_print_ptr(size_t ptr, int *tot_len);
+void	ft_print_char(char c, int *tot_len);
+void	ft_print_str(char *str, int *tot_len);
 #endif
