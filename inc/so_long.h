1772b3176eca7f00109276a642af04f9f8fa7840
balbader <basilalbapro@gmail.com>
2023-06-26 16:53:14 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -13,18 +13,22 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include "../mlx_linux/mlx.h"
-#include <X11/keysym.h>
-#include <X11/X.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <math.h>
-#include <errno.h>
-#include <string.h>
-#include <fcntl.h>
+# include "../mlx_linux/mlx.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+# include <errno.h>
+# include <string.h>
+# include <fcntl.h>
 # include <stddef.h>
 # include <limits.h>
 # include <stdarg.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 typedef struct t_start
 {
@@ -58,7 +62,6 @@ void	adding_in_graphics(t_complete *game);
 void	place_images_in_game(t_complete *game);
 void	check_errors(t_complete *game);
 
-int		main(void);
 int		ft_printf(const char *str, ...);
 void	ft_print_nb(int nb, int *tot_len);
 void	ft_print_hexa(unsigned int nb, char c, int *tot_len);
@@ -66,4 +69,12 @@ void	ft_print_unsigned(unsigned int u, int *tot_len);
 void	ft_print_ptr(size_t ptr, int *tot_len);
 void	ft_print_char(char c, int *tot_len);
 void	ft_print_str(char *str, int *tot_len);
+
+int		ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *str);
+char	*get_next_line(int fd);
+
 #endif
