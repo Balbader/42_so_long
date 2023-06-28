78f3e66fd0bb9b9cf545ac34f6442a397a5a1d32
Basil ALBADER <baalbade@student.42.fr>
2023-06-28 12:49:17 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/30 19:40:39 by baalbade          #+#    #+#             */
-/*   Updated: 2023/04/30 19:40:43 by baalbade         ###   ########.fr       */
+/*   Created: 2023/06/28 12:32:32 by baalbade          #+#    #+#             */
+/*   Updated: 2023/06/28 12:32:34 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,73 +14,194 @@
 # define SO_LONG_H
 
 # include "../mlx_linux/mlx.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
 # include <X11/keysym.h>
 # include <X11/X.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <math.h>
-# include <errno.h>
-# include <string.h>
 # include <fcntl.h>
-# include <stddef.h>
-# include <limits.h>
-# include <stdarg.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-typedef struct t_start
+typedef struct img_s
 {
+	void	*img_floor;
+	void	*img_player;
+	void	*img_exit;
+	void	*img_wall;
+	void	*img_collect;
+	int		height;
+	int		width;
+	char	*player;
+	char	*floor;
+	char	*wall;
+	char	*collect;
+	char	*exit;
+}				t_img;
+
+typedef struct cnt_s
+{
+	char	exit;
+	char	collect;
+	char	player;
+	char	wall;
+	char	space;
+	int		count_p;
+	int		count_e;
+	int		count_c;
+}				t_cnt;
+
+typedef struct pos_s
+{
+	int		x;
+	int		y;
+}				t_pos;
+
+typedef struct data_s
+{
+	void	*mlx_ptr;
+	void	*mlx_win;
+	int		width;
+	int		height;
 	char	**map;
+	t_cnt	content;
+	t_img	img;
+	t_pos	pos;
+	int		count;
+}				t_data;
 
-	void	*mlxpointer;
-	void	*winpointer;
-	void	*floor;
-	void	*barrier;
-	void	*player;
-	void	*exit;
-	void	*collectable;
+int		ft_strchr(char *str, char *cmp);
+void	*ft_error(char *str);
+int		ft_same_char(char *str);
 
-	int		fd;
-	int		heightmap;
-	int		widthmap;
-	int		playercount;
-	int		columncount;
-	int		exitcount;
-	int		x_axis;
-	int		y_axis;
-	int		counter;
-	int		collectables;
-}				t_complete;
+int		ft_same_char(char *str);
+char	*get_map(int fd);
+char	**map_core(char **str, t_data *data);
+int		chk_map(char **argv);
 
-int		exit_point(t_complete *game);
-int		map_reading(t_complete *game, char **argv);
-int		controls_working(int command, t_complete *game);
-void	adding_in_graphics(t_complete *game);
-void	place_images_in_game(t_complete *game);
-void	check_errors(t_complete *game);
+int		ft_check_col(char *map_line, char col, t_data *data);
+int		ft_check_line(char *map_line, char wall);
+int		ft_check_other(char *map_line, t_cnt *content);
+void	ft_check_content(t_data *data);
+int		ft_check_format(char **map);
+int		chk_collect(t_data *data);
 
-int		ft_printf(const char *str, ...);
-void	ft_print_nb(int nb, int *tot_len);
-void	ft_print_hexa(unsigned int nb, char c, int *tot_len);
-void	ft_print_unsigned(unsigned int u, int *tot_len);
-void	ft_print_ptr(size_t ptr, int *tot_len);
-void	ft_print_char(char c, int *tot_len);
-void	ft_print_str(char *str, int *tot_len);
+void	set_img(t_data *data);
+void	set_content(t_cnt *content);
 
-size_t	ft_strlen(const char *str);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *str);
-char	*get_next_line(int fd);
-char	*ft_strncpy(char *str, int nb);
-char	**ft_split(const char *s, char c);
-char	*ft_itoa(int n);
-void	ft_putstr(char *str);
-void	ft_putchar(char c);
-int		ft_isdigit(int c);
-int		ft_atoi(const char *str);
+void	core_render(t_data *data);
+int		render(t_data *data);
+void	render_other(t_data *data);
+void	render_background(t_data *data);
+void	render_down(t_data *data);
+void	render_left(t_data *data);
+void	render_right(t_data *data);
+void	render_top(t_data *data);
+void	print_img(t_data *data, void *img, int x, int y);
+
+int		key_press(int keysym, t_data *data);
+int		chk_collect(t_data *data);
+int		end(t_data *data);
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../minilibx-linux/mlx.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <fcntl.h>
+
+typedef struct img_s
+{
+	void	*img_floor;
+	void	*img_player;
+	void	*img_exit;
+	void	*img_wall;
+	void	*img_collect;
+	int		height;
+	int		width;
+	char	*player;
+	char	*floor;
+	char	*wall;
+	char	*collect;
+	char	*exit;
+}				t_img;
+
+typedef struct cnt_s
+{
+	char	exit;
+	char	collect;
+	char	player;
+	char	wall;
+	char	space;
+	int		count_p;
+	int		count_e;
+	int		count_c;
+}				t_cnt;
+
+typedef struct pos_s
+{
+	int		x;
+	int		y;
+}				t_pos;
+
+typedef struct data_s
+{
+	void	*mlx_ptr;
+	void	*mlx_win;
+	int		width;
+	int		height;
+	char	**map;
+	t_cnt	content;
+	t_img	img;
+	t_pos	pos;
+	int		count;
+}				t_data;
+
+// utils.c
+void			*ft_error(char *str);
+int				ft_same_char(char *str);
+
+// map_checker.c
+int				ft_check_col(char *map_line, char wall, t_data *data);
+int				ft_check_line(char *map_line, char wall);
+int				ft_check_other(char *map_line, t_cnt *content);
+void			ft_check_content(t_data *data);
+int				ft_check_format(char **map);
+
+// map.c
+char			*get_map(int fd);
+void			*ft_free_map(t_data *data);
+char			**parse_map(int fd, t_data *data);
+char			**map_core(char **str, t_data *data);
+
+// render.c
+void			render_background(t_data *data);
+void			render_other(t_data *data);
+int				render(t_data *data);
+void			core_render(t_data *data);
+void			print_img(t_data *data, void *img, int x, int y);
+
+// render_mouv.c
+void			render_top(t_data *data);
+void			render_right(t_data *data);
+void			render_left(t_data *data);
+void			render_down(t_data *data);
+
+// set.c
+void			set_content(t_cnt *content);
+void			set_img(t_data *data);
+
+// texture.c
+int				chk_collect(t_data *data);
+int				end(t_data *data);
+int				key_press(int keysym, t_data *data);
+
+//main.c
+int				main(int argc, char **argv);
+
 
 #endif
