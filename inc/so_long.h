369dc413902b8c5e9e2980db86b0b3705915ec23
Basil ALBADER <baalbade@student.42.fr>
2023-06-29 10:28:22 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -13,7 +13,6 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "../libft/includes/libft.h"
 # include "../mlx_linux/mlx.h"
 # include <stdlib.h>
 # include <unistd.h>
@@ -73,6 +72,17 @@ typedef struct data_s
 // utils.c
 int				ft_is_duplicate(char *str);
 void			*ft_print_error(char *str);
+long long int	ft_atoi(const char *str);
+size_t			ft_strlen(const char *str);
+int				ft_strchr(char *str, char *cmp);
+int				ft_isdigit(int c);
+void			ft_putchar(char c);
+void			ft_putstr(char *str);
+char			*ft_itoa(int n);
+char			**ft_split(char *str, char sep);
+char			*ft_strdup(char *str);
+char			*ft_strncpy(char *str, int nb);
+char			*ft_strjoin(char *s1, char *s2);
 
 // map_checker.c
 int				ft_check_column(char *map_line, char wall, t_data *data);
@@ -91,7 +101,7 @@ void			*ft_free_map(t_data *data);
 int				ft_render(t_data *data);
 void			ft_render_background(t_data *data);
 void			ft_render_other(t_data *data);
-void			ft_core_render(t_data *data);
+void			ft_render_main(t_data *data);
 void			ft_print_img(t_data *data, void *img, int x, int y);
 
 // render_mouv.c
@@ -111,5 +121,18 @@ int				ft_key_press(int keysym, t_data *data);
 
 //main.c
 int				main(int argc, char **argv);
+
+// ft_printf
+int				ft_printf(const char *str, ...);
+void			ft_print_nb(int nb, int *tot_len);
+void			ft_print_hexa(unsigned int nb, char c, int *tot_len);
+void			ft_print_unsigned(unsigned int u, int *tot_len);
+void			ft_print_ptr(size_t ptr, int *tot_len);
+void			ft_print_char(char c, int *tot_len);
+void			ft_print_str(char *str, int *tot_len);
+
+// gnl
+char			*ft_stradd(char *str, char buff);
+int				ft_get_next_line(int fd, char **str);
 
 #endif
