b817e10bd7aa1ded69062452947fc154cdd75d21
Basil ALBADER <baalbade@student.42.fr>
2023-06-29 10:28:56 +0200

    gnl functions

---
+++ srcs/gnl/ft_get_next_line.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_line.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 13:19:56 by baalbade          #+#    #+#             */
+/*   Updated: 2023/06/28 13:19:58 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+char	*ft_stradd(char *str, char buff)
+{
+	int		i;
+	char	*ret;
+
+	i = 0;
+	ret = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2));
+	if (ret == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		ret[i] = str[i];
+		i++;
+	}
+	free(str);
+	ret[i] = buff;
+	ret[++i] = '\0';
+	return (ret);
+}
+
+int	ft_get_next_line(int fd, char **str)
+{
+	char			buff;
+	int				ret;
+
+	ret = read(fd, &buff, 1);
+	while (ret > 0)
+	{
+		*str = ft_stradd(*str, buff);
+		if (buff == '\n')
+			return (ret);
+		else
+			ret += 1;
+		ret = read(fd, &buff, 1);
+	}
+	if (ret == 0)
+	{
+		free(*str);
+		*str = NULL;
+	}
+	return (ret);
+}
