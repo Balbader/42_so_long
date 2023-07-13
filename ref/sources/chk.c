fcdc1aab1842a20ad1382c762f0ad042fab31d71
Basil ALBADER <baalbade@student.42.fr>
2023-07-13 09:56:46 +0200

    ref functions

---
+++ ref/sources/chk.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chk.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emis <emis@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 17:29:44 by emis              #+#    #+#             */
+/*   Updated: 2023/01/19 12:04:27 by emis             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "gui.h"
+
+static int	propaganda(char **fillmap, int x, int y)
+{
+	int	filled;
+
+	filled = 0;
+	if (map2typ(fillmap[y - 1][x]) != wall
+		&& fillmap[y - 1][x] != 42 && ++filled)
+		fillmap[y - 1][x] = 42;
+	if (map2typ(fillmap[y + 1][x]) != wall
+		&& fillmap[y + 1][x] != 42 && ++filled)
+		fillmap[y + 1][x] = 42;
+	if (map2typ(fillmap[y][x - 1]) != wall
+		&& fillmap[y][x - 1] != 42 && ++filled)
+		fillmap[y][x - 1] = 42;
+	if (map2typ(fillmap[y][x + 1]) != wall
+		&& fillmap[y][x + 1] != 42 && ++filled)
+		fillmap[y][x + 1] = 42;
+	return (filled);
+}
+
+static char	**copy_map(char **map, int size)
+{
+	char	**cpy;
+	int		i;
+
+	cpy = ft_calloc(sizeof(char *), size);
+	i = -1;
+	while (++i < size)
+		cpy[i] = ft_strdup(map[i]);
+	return (cpy);
+}
+
+int	printmap(char **map_copy)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (map_copy[++i])
+	{
+		j = -1;
+		while (map_copy[i][++j])
+			ft_printf("%c", map_copy[i][j]);
+		ft_printf("\n");
+	}
+	ft_printf("\n");
+	return (1);
+}
+
+int	check_map(t_gui *gui, int status)
+{
+	static char	**fillmap;
+	int			filled;
+	int			x;
+	int			y;
+
+	x = gui->map_w;
+	y = gui->map_h;
+	if (!status && (count(gui->map, y, x, collectible) < 1 || count(gui->map,
+				y, x, player) != 1 || count(gui->map, y, x, map_exit) != 1))
+		return (err(map_invalid_error));
+	if (!fillmap)
+		fillmap = copy_map(gui->map, gui->map_h);
+	filled = status;
+	x = 0;
+	y = 0;
+	while (++y < gui->map_h)
+		while (++x % (gui->map_w + 1) < gui->map_w)
+			if (map2typ(fillmap[y][x % (gui->map_w + 1)]) >= player)
+				filled += propaganda(fillmap, x % (gui->map_w + 1), y);
+	if (filled != status)
+		return (check_map(gui, filled));
+	if (count(fillmap, y, gui->map_w, collectible)
+		|| count(fillmap, y, gui->map_w, map_exit))
+		return (destroy_map(fillmap, gui->map_h), err(map_no_win_error));
+	return (destroy_map(fillmap, gui->map_h), 0);
+}
+
+int	check_move(t_kbind key, t_gui *gui)
+{
+	int		xtarg;
+	int		ytarg;
+	int		ttarg;
+
+	xtarg = (key == right) - (key == left);
+	ytarg = (key == down) - (key == up);
+	ttarg = gui->map[ytarg + (gui->player->y / 32)]
+	[xtarg + (gui->player->x / 32)];
+	if (map2typ(ttarg) == wall)
+		return (0);
+	else if (BONUS && enemy_at(gui, (xtarg * 32) + gui->player->x,
+			(ytarg * 32) + gui->player->y, 1))
+		msg_event(gui, died_event);
+	else if (map2typ(ttarg) == collectible)
+	{
+		gui->to_collect--;
+		msg_event(gui, collected_event);
+		gui->map[ytarg + (gui->player->y / 32)]
+		[xtarg + (gui->player->x / 32)] = '0';
+	}
+	else if (map2typ(ttarg) == map_exit)
+		if (!gui->to_collect)
+			msg_event(gui, win_event);
+	return (1);
+}
---
+++ ref/sources/drw.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   drw.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emis <emis@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 16:50:56 by emis              #+#    #+#             */
+/*   Updated: 2023/01/19 13:23:21 by emis             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "gui.h"
+
+void	top_line_redraw(t_gui *gui)
+{
+	int		x;
+
+	x = -1;
+	while (++x < gui->map_w)
+		mlx_put_image_to_window(gui->mlx, gui->mlx->win_list,
+			(gui->el[wall]->img_list)[0], x * 32, 0);
+}
+
+int	background_redraw(t_gui *gui)
+{
+	int		x;
+	int		y;
+	t_els	*up;
+
+	x = -1;
+	y = -1;
+	while (++y < gui->map_h)
+	{
+		while (++x % (gui->map_w + 1) < gui->map_w)
+		{
+			up = gui->el[map2typ(gui->map[y][x % (gui->map_w + 1)])];
+			while (up)
+			{
+				mlx_put_image_to_window(gui->mlx, gui->mlx->win_list,
+					(up->img_list)[up->img], x % (gui->map_w + 1) * 32,
+					y * 32);
+				up = up->next;
+			}
+		}
+	}
+	mlx_put_image_to_window(gui->mlx, gui->mlx->win_list,
+		(gui->el[player]->img_list)[gui->el[player]->img],
+		gui->el[player]->x, gui->el[player]->y);
+	gui->to_update = 1;
+	return (0);
+}
+
+void	tile_redraw(t_gui *gui, int x, int y)
+{
+	t_els	*up;
+
+	up = gui->el[map2typ(gui->map[y][x])];
+	mlx_put_image_to_window(gui->mlx, gui->mlx->win_list,
+		(up->img_list)[up->img], x * 32, y * 32);
+}
+
+void	point_draw(t_gui *gui, int x, int y, int size)
+{
+	int		i;
+	int		width;
+
+	i = -1;
+	width = (2 * size) + 1;
+	while (++i < width * width)
+		mlx_pixel_put(gui->mlx, gui->mlx->win_list,
+			x + (i % width) - size, y + (i / width) - size, 0xFFFFFF);
+}
+
+void	enemies_redraw(t_gui *gui)
+{
+	t_els	*en;
+
+	en = gui->el[enemy];
+	while (en)
+	{
+		if ((en->x != gui->player->x || en->y != gui->player->y)
+			&& (en->img != en->img_list_size - 1
+				|| map2typ(gui->map[en->y / 32][en->x / 32]) == background))
+			mlx_put_image_to_window(gui->mlx, gui->mlx->win_list,
+				(en->img_list)[en->img], en->x, en->y);
+		else if (en->x != gui->player->x || en->y != gui->player->y)
+			tile_redraw(gui, en->x / 32, en->y / 32);
+		en = en->next;
+	}
+}
