6d5b917d58d658981940f684c28bf68916e4537a
balbader <basilalbapro@gmail.com>
2023-08-03 17:15:51 +0200

    w

---
+++ inc/so_long.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 16:46:03 by baalbade          #+#    #+#             */
+/*   Updated: 2023/08/03 16:46:05 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "mlx_linux/mlx.h"
+# include "libft/libft.h"
+# include "structures.h"
+# include "define.h"
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <X11/X.h>
+
+#endif // !SO_LONG_H
