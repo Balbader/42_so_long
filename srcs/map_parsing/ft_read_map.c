e2bb03fb765cc3690feb96feb95df22841b8615d
balbader <basilalbapro@gmail.com>
2023-08-04 17:50:43 +0200

    ft_read_map.c

---
+++ srcs/map_parsing/ft_read_map.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read_map.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 11:38:38 by baalbade          #+#    #+#             */
+/*   Updated: 2023/08/04 11:38:39 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+char	*read_map(int fd)
+{
+	char	*line;
+	char	*text;
+	char	*temp;
+
+	text = ft_strdup("");
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		temp = ft_strjoin(text, line);
+		ft_free_multiple(2, text, line);
+		text = temp;
+		line = get_next_line(fd);
+	}
+	return (text);
+}
