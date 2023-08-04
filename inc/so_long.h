7134b32b4ac8d0cbd7c1abf54e59f687a80bb75b
balbader <basilalbapro@gmail.com>
2023-08-04 10:47:36 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -14,7 +14,6 @@
 # define SO_LONG_H
 
 # include "mlx_linux/mlx.h"
-# include "libft/libft.h"
 # include "structures.h"
 # include "define.h"
 
@@ -22,5 +21,19 @@
 # include <stdio.h>
 #include <stddef.h>
 # include <X11/X.h>
+
+/* GNL */
+char	*get_next_line(int fd);
+char	*strjoin(char *line, char *buffer);
+bool	ft_manage_buffer(char *buffer);
+
+/* ft_printf */
+int		ft_printf(const char *str, ...);
+void	ft_print_nb(int nb, int *tot_len);
+void	ft_print_hexa(unsigned int nb, char c, int *tot_len);
+void	ft_print_unsigned(unsigned int u, int *tot_len);
+void	ft_print_ptr(size_t ptr, int *tot_len);
+void	ft_print_char(char c, int *tot_len);
+void	ft_print_str(char *str, int *tot_len);
 
 #endif // !SO_LONG_H
