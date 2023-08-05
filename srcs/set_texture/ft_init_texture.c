67a0c0935549094d981d26def23eae92496c4742
balbader <basilalbapro@gmail.com>
2023-08-05 08:37:37 +0200

    ft_init_textures.c

---
+++ srcs/set_texture/ft_init_texture.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init_texture.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/05 08:26:21 by baalbade          #+#    #+#             */
+/*   Updated: 2023/08/05 08:26:24 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_init_texture(t_vars *vars)
+{
+
+	ft_set_wall(vars);
+	ft_set_pc_up(vars);
+	ft_set_pc_down(vars);
+	ft_set_pc_left(vars);
+	ft_set_pc_right(vars);
+	ft_set_map_exit(vars);
+	ft_set_empty_space(vars); // rename to floor
+	ft_set_collectibles(vars);
+	ft_set_player(vars);
+}
