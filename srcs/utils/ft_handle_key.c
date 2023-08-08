91b38cc18e426653061ad73407de71953f108c27
balbader <basilalbapro@gmail.com>
2023-08-08 13:34:09 +0200

    cleanup

--- srcs/utils/ft_handle_key.c
+++ srcs/utils/ft_handle_key.c
@@ -15,7 +15,7 @@
 int	ft_handle_key(int key, t_vars *vars)
 {
 	if (key == ESCAPE)
-		close_window(vars);
+		ft_close_window(vars);
 	if (key == W || key == UP)
 		vars->player.move.up = 1;
 	if (key == S || key == DOWN)
