50d4deaa15c1eb854fa97ca35e45d0448c5d4e0e
balbader <basilalbapro@gmail.com>
2023-08-08 15:12:01 +0200

    update

--- srcs/set_texture/ft_init_textures_utils2.c
+++ srcs/set_texture/ft_init_textures_utils2.c
@@ -15,29 +15,29 @@
 void	ft_set_pc_up(t_vars *vars)
 {
 	vars->map.pc_up = ft_load_texture_to_img(vars, \
-	"assets/textures/XPM/PC_UP.xpm");
+	"resources/textures/XPM/PC_UP.xpm");
 }
 
 void	ft_set_pc_down(t_vars *vars)
 {
 	vars->map.pc_down = ft_load_texture_to_img(vars, \
-	"assets/textures/XPM/PC_DOWN.xpm");
+	"resources/textures/XPM/PC_DOWN.xpm");
 }
 
 void	ft_set_pc_left(t_vars *vars)
 {
 	vars->map.pc_left = ft_load_texture_to_img(vars, \
-	"assets/textures/XPM/PC_LEFT.xpm");
+	"resources/textures/XPM/PC_LEFT.xpm");
 }
 
 void	ft_set_pc_right(t_vars *vars)
 {
 	vars->map.pc_right = ft_load_texture_to_img(vars, \
-	"assets/textures/XPM/PC_RIGHT.xpm");
+	"resources/textures/XPM/PC_RIGHT.xpm");
 }
 
 void	ft_set_player(t_vars *vars)
 {
 	vars->player.img = ft_load_texture_to_img(vars, \
-	"assets/textures/PLAYER/XPM/NORMAL/1.xpm");
+	"resources/textures/PLAYER/XPM/NORMAL/1.xpm");
 }
