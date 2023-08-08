50dec9881d5fee4bfa7fd6c3f4f6c540b78f64d1
balbader <basilalbapro@gmail.com>
2023-08-08 17:03:16 +0200

    WIP

--- srcs/libft/ft_split.c
+++ srcs/libft/ft_split.c
@@ -12,68 +12,67 @@
 
 #include "so_long.h"
 
-static size_t	ft_count_words(char const *str, char split)
+int	count_words(const char *str, char c)
 {
-	size_t	res;
+	size_t	word;
+
+	word = 0;
+	while (*str)
+	{
+		if (*str != c)
+		{
+			word++;
+			while (*str != c && *str)
+				str++;
+		}
+		else
+			str++;
+	}
+	return (word);
+}
+
+char	*malloc_word(const char *s, char c)
+{
+	char	*word;
 	size_t	i;
 
-	res = 0;
-	i = 0;
-	while (str[i])
-	{
-		while (str[i] && str[i] == split)
-			++i;
-		if (str[i])
-			++res;
-		while (str[i] && str[i] != split)
-			++i;
-	}
-	return (res);
-}
-
-static char	*ft_spe_strdup(char const *s, char c)
-{
-	int		i;
-	char	*dst;
-
 	i = 0;
 	while (s[i] && s[i] != c)
 		i++;
-	dst = (char *)malloc(sizeof(char) * (i + 1));
-	if (!dst)
+	word = (char *)malloc(sizeof(char) * (i + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t		i;
+	char		**splited;
+
+	splited = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!splited || !s)
 		return (0);
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (*s)
 	{
-		dst[i] = s[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	char	**tab;
-	int		word_count;
-	int		i;
-	int		j;
-
-	word_count = ft_count_words((char *)s, c);
-	tab = (char **)malloc(sizeof(char *) * (word_count + 1));
-	if (!tab)
-		return (NULL);
-	tab[word_count] = NULL;
-	i = 0;
-	j = 0;
-	while (j < word_count)
-	{
-		while (s[i] && s[i] == c)
+		while (*s && *s == c)
+			s++;
+		if (*s && *s != c)
+		{
+			splited[i] = malloc_word(s, c);
 			i++;
-		tab[j++] = ft_spe_strdup(s + i, c);
-		while (s[i] && s[i] != c)
-			i++;
+			while (*s && *s != c)
+				s++;
+		}
 	}
-	tab[j] = NULL;
-	return (tab);
+	splited[i] = 0;
+	return (splited);
 }
