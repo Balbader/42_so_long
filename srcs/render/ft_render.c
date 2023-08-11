a0f093a4f21a6b97526689eace92922fc34c5726
balbader <basilalbapro@gmail.com>
2023-08-11 13:16:58 +0200

    ft_render.c

--- srcs/render/ft_render.c
+++ srcs/render/ft_render.c
@@ -20,10 +20,5 @@ int	ft_render(t_vars *vars)
 	ft_confirm_move(vars);
 	ft_render_player(vars);
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->final_image.img, 0, 0);
-	/*
-	ft_print_collectibles(vars, (t_pos){10, 20}, vars->map.collectible_count, \
-	vars->player.collectible_count);
-	ft_print_moves(vars, (t_pos){10, 40}, vars->player.move_count);
-	*/
 	return (EXIT_SUCCESS);
 }
