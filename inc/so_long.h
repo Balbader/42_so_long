3e7707ac37cb074fb2d466d68910f35c92ff0a63
Basil ALBADER <baalbade@student.42.fr>
2023-08-16 13:10:40 +0200

    .h

--- inc/so_long.h
+++ inc/so_long.h
@@ -105,6 +105,7 @@ char			*ft_itoa(int n);
 char			*ft_strdup(const char *str);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			**ft_split(const char *s, char c);
+void			ft_free_map(char *map);
 void			ft_free_tab(char **tab);
 void			ft_free_tabs(unsigned int count, ...);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
